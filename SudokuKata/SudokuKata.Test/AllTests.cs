9f8beddce05a0917beefe1c99b4968d4fdba2f8b
Tony Vo <ttrung.vo@gmail.com>
2021-04-17 13:34:21 -0600

    setup approval tests

---
+++ SudokuKata/SudokuKata.Test/AllTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SudokuKata.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SampleTestShouldFail()
+        {
+            Assert.Fail();
+        }
+    }
+}
