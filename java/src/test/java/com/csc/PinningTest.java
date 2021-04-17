526a52bbabeb69da0aa6ec60032d040360a14c0c
Tony Vo <ttrung.vo@gmail.com>
2021-04-17 12:59:15 -0600

    add java version of sudoku

--- java/src/test/java/com/csc/PinningTest.java
+++ java/src/test/java/com/csc/PinningTest.java
@@ -4,12 +4,12 @@ import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class PinningTest {
 
     @Test
-    void pinEverything() {
-        Integer [] dice = new Integer [] {1, 2, 3, 4, 5, 6};
-        CombinationApprovals.verifyAllCombinations(BigDiceGame::CalculateScore, ScoringType.values(), dice, dice, dice, dice, dice);
+    void sampleTestShouldFail() {
+        fail();
     }
 }
