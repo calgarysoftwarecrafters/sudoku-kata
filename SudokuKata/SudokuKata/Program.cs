9b4a44b97cb7e096d510bd889db9d38578072db4
Tony Vo <ttrung.vo@gmail.com>
2021-04-18 07:53:08 -0600

    r extract AppleSauce4

--- SudokuKata/SudokuKata/Program.cs
+++ SudokuKata/SudokuKata/Program.cs
@@ -52,137 +52,7 @@ namespace SudokuKata
             string command = "expand";
             while (stateStack.Count <= 9 * 9)
             {
-                if (command == "expand")
-                {
-                    int[] currentState = new int[9 * 9];
-
-                    if (stateStack.Count > 0)
-                    {
-                        Array.Copy(stateStack.Peek(), currentState, currentState.Length);
-                    }
-
-                    int bestRow = -1;
-                    int bestCol = -1;
-                    bool[] bestUsedDigits = null;
-                    int bestCandidatesCount = -1;
-                    int bestRandomValue = -1;
-                    bool containsUnsolvableCells = false;
-
-                    for (int index = 0; index < currentState.Length; index++)
-                        if (currentState[index] == 0)
-                        {
-
-                            int row = index / 9;
-                            int col = index % 9;
-                            int blockRow = row / 3;
-                            int blockCol = col / 3;
-
-                            bool[] isDigitUsed = new bool[9];
-
-                            for (int i = 0; i < 9; i++)
-                            {
-                                int rowDigit = currentState[9 * i + col];
-                                if (rowDigit > 0)
-                                    isDigitUsed[rowDigit - 1] = true;
-
-                                int colDigit = currentState[9 * row + i];
-                                if (colDigit > 0)
-                                    isDigitUsed[colDigit - 1] = true;
-
-                                int blockDigit = currentState[(blockRow * 3 + i / 3) * 9 + (blockCol * 3 + i % 3)];
-                                if (blockDigit > 0)
-                                    isDigitUsed[blockDigit - 1] = true;
-                            }
-
-                            int candidatesCount = isDigitUsed.Where(used => !used).Count();
-
-                            if (candidatesCount == 0)
-                            {
-                                containsUnsolvableCells = true;
-                                break;
-                            }
-
-                            int randomValue = randomNumbers.Next();
-
-                            if (bestCandidatesCount < 0 ||
-                                candidatesCount < bestCandidatesCount ||
-                                (candidatesCount == bestCandidatesCount && randomValue < bestRandomValue))
-                            {
-                                bestRow = row;
-                                bestCol = col;
-                                bestUsedDigits = isDigitUsed;
-                                bestCandidatesCount = candidatesCount;
-                                bestRandomValue = randomValue;
-                            }
-
-                        }
-
-                    if (!containsUnsolvableCells)
-                    {
-                        stateStack.Push(currentState);
-                        rowIndexStack.Push(bestRow);
-                        colIndexStack.Push(bestCol);
-                        usedDigitsStack.Push(bestUsedDigits);
-                        lastDigitStack.Push(0); // No digit was tried at this position
-                    }
-
-                    // Always try to move after expand
-                    command = "move";
-
-                }
-                else if (command == "collapse")
-                {
-                    stateStack.Pop();
-                    rowIndexStack.Pop();
-                    colIndexStack.Pop();
-                    usedDigitsStack.Pop();
-                    lastDigitStack.Pop();
-
-                    command = "move";   // Always try to move after collapse
-                }
-                else if (command == "move")
-                {
-
-                    int rowToMove = rowIndexStack.Peek();
-                    int colToMove = colIndexStack.Peek();
-                    int digitToMove = lastDigitStack.Pop();
-
-                    int rowToWrite = rowToMove + rowToMove / 3 + 1;
-                    int colToWrite = colToMove + colToMove / 3 + 1;
-
-                    bool[] usedDigits = usedDigitsStack.Peek();
-                    int[] currentState = stateStack.Peek();
-                    int currentStateIndex = 9 * rowToMove + colToMove;
-
-                    int movedToDigit = digitToMove + 1;
-                    while (movedToDigit <= 9 && usedDigits[movedToDigit - 1])
-                        movedToDigit += 1;
-
-                    if (digitToMove > 0)
-                    {
-                        usedDigits[digitToMove - 1] = false;
-                        currentState[currentStateIndex] = 0;
-                        board[rowToWrite][colToWrite] = '.';
-                    }
-
-                    if (movedToDigit <= 9)
-                    {
-                        lastDigitStack.Push(movedToDigit);
-                        usedDigits[movedToDigit - 1] = true;
-                        currentState[currentStateIndex] = movedToDigit;
-                        board[rowToWrite][colToWrite] = (char)('0' + movedToDigit);
-
-                        // Next possible digit was found at current position
-                        // Next step will be to expand the state
-                        command = "expand";
-                    }
-                    else
-                    {
-                        // No viable candidate was found at current position - pop it in the next iteration
-                        lastDigitStack.Push(0);
-                        command = "collapse";
-                    }
-                }
+                command = AppleSauce4(randomNumbers, command, stateStack, rowIndexStack, colIndexStack, usedDigitsStack, lastDigitStack, board);
             }
 
             Console.WriteLine();
@@ -222,6 +92,140 @@ namespace SudokuKata
 
                 PrintBoardChange(changeMade, board);
             }
+        }
+
+        private static string AppleSauce4(Random randomNumbers, string command, Stack<int[]> stateStack, Stack<int> rowIndexStack,
+            Stack<int> colIndexStack, Stack<bool[]> usedDigitsStack, Stack<int> lastDigitStack, char[][] board)
+        {
+            if (command == "expand")
+            {
+                int[] currentState = new int[9 * 9];
+
+                if (stateStack.Count > 0)
+                {
+                    Array.Copy(stateStack.Peek(), currentState, currentState.Length);
+                }
+
+                int bestRow = -1;
+                int bestCol = -1;
+                bool[] bestUsedDigits = null;
+                int bestCandidatesCount = -1;
+                int bestRandomValue = -1;
+                bool containsUnsolvableCells = false;
+
+                for (int index = 0; index < currentState.Length; index++)
+                    if (currentState[index] == 0)
+                    {
+                        int row = index / 9;
+                        int col = index % 9;
+                        int blockRow = row / 3;
+                        int blockCol = col / 3;
+
+                        bool[] isDigitUsed = new bool[9];
+
+                        for (int i = 0; i < 9; i++)
+                        {
+                            int rowDigit = currentState[9 * i + col];
+                            if (rowDigit > 0)
+                                isDigitUsed[rowDigit - 1] = true;
+
+                            int colDigit = currentState[9 * row + i];
+                            if (colDigit > 0)
+                                isDigitUsed[colDigit - 1] = true;
+
+                            int blockDigit = currentState[(blockRow * 3 + i / 3) * 9 + (blockCol * 3 + i % 3)];
+                            if (blockDigit > 0)
+                                isDigitUsed[blockDigit - 1] = true;
+                        }
+
+                        int candidatesCount = isDigitUsed.Where(used => !used).Count();
+
+                        if (candidatesCount == 0)
+                        {
+                            containsUnsolvableCells = true;
+                            break;
+                        }
+
+                        int randomValue = randomNumbers.Next();
+
+                        if (bestCandidatesCount < 0 ||
+                            candidatesCount < bestCandidatesCount ||
+                            (candidatesCount == bestCandidatesCount && randomValue < bestRandomValue))
+                        {
+                            bestRow = row;
+                            bestCol = col;
+                            bestUsedDigits = isDigitUsed;
+                            bestCandidatesCount = candidatesCount;
+                            bestRandomValue = randomValue;
+                        }
+                    }
+
+                if (!containsUnsolvableCells)
+                {
+                    stateStack.Push(currentState);
+                    rowIndexStack.Push(bestRow);
+                    colIndexStack.Push(bestCol);
+                    usedDigitsStack.Push(bestUsedDigits);
+                    lastDigitStack.Push(0); // No digit was tried at this position
+                }
+
+                // Always try to move after expand
+                command = "move";
+            }
+            else if (command == "collapse")
+            {
+                stateStack.Pop();
+                rowIndexStack.Pop();
+                colIndexStack.Pop();
+                usedDigitsStack.Pop();
+                lastDigitStack.Pop();
+
+                command = "move"; // Always try to move after collapse
+            }
+            else if (command == "move")
+            {
+                int rowToMove = rowIndexStack.Peek();
+                int colToMove = colIndexStack.Peek();
+                int digitToMove = lastDigitStack.Pop();
+
+                int rowToWrite = rowToMove + rowToMove / 3 + 1;
+                int colToWrite = colToMove + colToMove / 3 + 1;
+
+                bool[] usedDigits = usedDigitsStack.Peek();
+                int[] currentState = stateStack.Peek();
+                int currentStateIndex = 9 * rowToMove + colToMove;
+
+                int movedToDigit = digitToMove + 1;
+                while (movedToDigit <= 9 && usedDigits[movedToDigit - 1])
+                    movedToDigit += 1;
+
+                if (digitToMove > 0)
+                {
+                    usedDigits[digitToMove - 1] = false;
+                    currentState[currentStateIndex] = 0;
+                    board[rowToWrite][colToWrite] = '.';
+                }
+
+                if (movedToDigit <= 9)
+                {
+                    lastDigitStack.Push(movedToDigit);
+                    usedDigits[movedToDigit - 1] = true;
+                    currentState[currentStateIndex] = movedToDigit;
+                    board[rowToWrite][colToWrite] = (char) ('0' + movedToDigit);
+
+                    // Next possible digit was found at current position
+                    // Next step will be to expand the state
+                    command = "expand";
+                }
+                else
+                {
+                    // No viable candidate was found at current position - pop it in the next iteration
+                    lastDigitStack.Push(0);
+                    command = "collapse";
+                }
+            }
+
+            return command;
         }
 
         private static Dictionary<int, int> PrepareLookupStructures(out Dictionary<int, int> singleBitToIndex, out int allOnes)
