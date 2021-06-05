62d7e9ba214fa76328304b4a23fcaf31bbe7931f
Tony Vo <ttrung.vo@gmail.com>
2021-06-05 15:25:02 -0600

    r remove unused set methods

--- SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
+++ SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
@@ -27,12 +27,12 @@ namespace SudokuKata
     public class Stacks
     {
         // Top elements are (row, col) of cell which has been modified compared to previous state
-        public Stack<int> RowIndexStack { get; private set; } = new Stack<int>();
-        public Stack<int> ColIndexStack { get; private set; } = new Stack<int>();
+        public Stack<int> RowIndexStack { get; } = new Stack<int>();
+        public Stack<int> ColIndexStack { get; } = new Stack<int>();
         // Top element indicates candidate digits (those with False) for (row, col)
-        public Stack<bool[]> UsedDigitsStack { get; private set; } = new Stack<bool[]>();
+        public Stack<bool[]> UsedDigitsStack { get; } = new Stack<bool[]>();
         // Top element is the value that was set on (row, col)
-        public Stack<int> LastDigitStack { get; private set; } = new Stack<int>();
+        public Stack<int> LastDigitStack { get; } = new Stack<int>();
     }
 
     public class SudokuBoardAndStackState
