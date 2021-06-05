489d26816948813022a84d5e633cc7e6964ca7c4
Tony Vo <ttrung.vo@gmail.com>
2021-06-05 17:05:00 -0600

    r move stacks to different file

---
+++ SudokuKata/SudokuKata/Stacks.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace SudokuKata
+{
+    public class Stacks
+    {
+        // Top elements are (row, col) of cell which has been modified compared to previous state
+        public Stack<int> RowIndexStack { get; } = new Stack<int>();
+        public Stack<int> ColIndexStack { get; } = new Stack<int>();
+        // Top element indicates candidate digits (those with False) for (row, col)
+        public Stack<bool[]> UsedDigitsStack { get; } = new Stack<bool[]>();
+        // Top element is the value that was set on (row, col)
+        public Stack<int> LastDigitStack { get; } = new Stack<int>();
+    }
+}
--- SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
+++ SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
@@ -4,17 +4,6 @@ using System.Linq;
 
 namespace SudokuKata
 {
-    public class Stacks
-    {
-        // Top elements are (row, col) of cell which has been modified compared to previous state
-        public Stack<int> RowIndexStack { get; } = new Stack<int>();
-        public Stack<int> ColIndexStack { get; } = new Stack<int>();
-        // Top element indicates candidate digits (those with False) for (row, col)
-        public Stack<bool[]> UsedDigitsStack { get; } = new Stack<bool[]>();
-        // Top element is the value that was set on (row, col)
-        public Stack<int> LastDigitStack { get; } = new Stack<int>();
-    }
-
     public class SudokuBoardAndStackState
     {
         public SudokuBoardAndStackState()
