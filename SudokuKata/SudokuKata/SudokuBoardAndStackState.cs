37acccc08799434bcce9fe8a080ab60c07323575
Tony Vo <ttrung.vo@gmail.com>
2021-06-05 15:23:57 -0600

    r move assignment to initialization for private variables

--- SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
+++ SudokuKata/SudokuKata/SudokuBoardAndStackState.cs
@@ -28,19 +28,14 @@ namespace SudokuKata
     {
         public Stacks()
         {
-            // Top elements are (row, col) of cell which has been modified compared to previous state
-            RowIndexStack = new Stack<int>();
-            ColIndexStack = new Stack<int>();
-            // Top element indicates candidate digits (those with False) for (row, col)
-            UsedDigitsStack = new Stack<bool[]>();
-            // Top element is the value that was set on (row, col)
-            LastDigitStack = new Stack<int>();
         }
-
-        public Stack<int> RowIndexStack { get; private set; }
-        public Stack<int> ColIndexStack { get; private set; }
-        public Stack<bool[]> UsedDigitsStack { get; private set; }
-        public Stack<int> LastDigitStack { get; private set; }
+        // Top elements are (row, col) of cell which has been modified compared to previous state
+        public Stack<int> RowIndexStack { get; private set; } = new Stack<int>();
+        public Stack<int> ColIndexStack { get; private set; } = new Stack<int>();
+        // Top element indicates candidate digits (those with False) for (row, col)
+        public Stack<bool[]> UsedDigitsStack { get; private set; } = new Stack<bool[]>();
+        // Top element is the value that was set on (row, col)
+        public Stack<int> LastDigitStack { get; private set; } = new Stack<int>();
     }
 
     public class SudokuBoardAndStackState
